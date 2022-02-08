c4f548eb1da658e7fdf5ff207e440c9c0125c1a7
bcollinsStevens <bcollins@Stevens.edu>
2022-02-08 18:57:10 -0500

    Wrote the TUI interface as an object with methods, Wrote main loop calling those methods

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+mod chat_interface; // Tell rust to look for chat_interface.rs or chat_interface/mod.rs and import it under the name chat_interface
+use chat_interface::ChatInterface; // Use ChatInterface from our module, chat_interface
+
+use std::{
+    error::{
+        Error
+    }
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+
+    let mut interface = ChatInterface::new()?;
+    interface.init()?;
+    interface.push_message(String::from("Welcome to the Chat"));
+
+    loop {
+        interface.draw()?;
+        interface.do_input()?;
+        if interface.check_quit() {
+            interface.dinit()?;
+            break;
+        }
+    }
+
+    Ok(())
 }
